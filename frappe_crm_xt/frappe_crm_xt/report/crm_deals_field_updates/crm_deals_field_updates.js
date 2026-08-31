7d0782ea019b8a69c14b37f3bd10463bc1b55f72
trufurs <rajsinghjain.jp@gmail.com>
2026-08-31 06:49:39 +0000

    fix: report permission roles to sales manager only

---
+++ frappe_crm_xt/frappe_crm_xt/report/crm_deals_field_updates/crm_deals_field_updates.js
@@ -0,0 +1,13 @@
+// Copyright (c) 2026, rtcamp and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["CRM Deals Field Updates"] = {
+	filters: [
+		// {
+		// 	"fieldname": "my_filter",
+		// 	"label": __("My Filter"),
+		// 	"fieldtype": "Data",
+		// 	"reqd": 1,
+		// },
+	],
+};
---
+++ frappe_crm_xt/frappe_crm_xt/report/crm_lead_conversion_rate/crm_lead_conversion_rate.js
@@ -0,0 +1,13 @@
+// Copyright (c) 2026, rtcamp and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["CRM Lead Conversion Rate"] = {
+	filters: [
+		// {
+		// 	"fieldname": "my_filter",
+		// 	"label": __("My Filter"),
+		// 	"fieldtype": "Data",
+		// 	"reqd": 1,
+		// },
+	],
+};
