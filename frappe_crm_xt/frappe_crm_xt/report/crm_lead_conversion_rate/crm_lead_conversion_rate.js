f2a41e2d5b4349e3bf78116ecae5db2fd9af7ad8
trufurs <rajsinghjain.jp@gmail.com>
2026-09-01 11:39:21 +0000

    fix: add filters for year and month to CRM Lead Conversion Rate report

--- frappe_crm_xt/frappe_crm_xt/report/crm_lead_conversion_rate/crm_lead_conversion_rate.js
+++ frappe_crm_xt/frappe_crm_xt/report/crm_lead_conversion_rate/crm_lead_conversion_rate.js
@@ -1,4 +1,19 @@
 // Copyright (c) 2026, rtcamp and contributors
 // For license information, please see license.txt
 
-frappe.query_reports["CRM Lead Conversion Rate"] = {};
+frappe.query_reports["CRM Lead Conversion Rate"] = {
+	filters: [
+		{
+			fieldname: "year",
+			label: __("Fiscal Year"),
+			fieldtype: "Link",
+			options: "Fiscal Year",
+		},
+		{
+			fieldname: "month",
+			label: __("Month"),
+			fieldtype: "Select",
+			options: ["", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"],
+		},
+	],
+};
