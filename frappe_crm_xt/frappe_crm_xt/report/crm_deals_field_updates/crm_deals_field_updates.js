acee8865169f0d0aaa2530b8a3c86b2b028762a7
trufurs <rajsinghjain.jp@gmail.com>
2026-09-01 11:16:13 +0000

    fix: boilerplate bug

--- frappe_crm_xt/frappe_crm_xt/report/crm_deals_field_updates/crm_deals_field_updates.js
+++ frappe_crm_xt/frappe_crm_xt/report/crm_deals_field_updates/crm_deals_field_updates.js
@@ -1,13 +1,4 @@
 // Copyright (c) 2026, rtcamp and contributors
 // For license information, please see license.txt
 
-frappe.query_reports["CRM Deals Field Updates"] = {
-	filters: [
-		// {
-		// 	"fieldname": "my_filter",
-		// 	"label": __("My Filter"),
-		// 	"fieldtype": "Data",
-		// 	"reqd": 1,
-		// },
-	],
-};
+frappe.query_reports["CRM Deals Field Updates"] = {};
--- frappe_crm_xt/frappe_crm_xt/report/crm_lead_conversion_rate/crm_lead_conversion_rate.js
+++ frappe_crm_xt/frappe_crm_xt/report/crm_lead_conversion_rate/crm_lead_conversion_rate.js
@@ -1,13 +1,4 @@
 // Copyright (c) 2026, rtcamp and contributors
 // For license information, please see license.txt
 
-frappe.query_reports["CRM Lead Conversion Rate"] = {
-	filters: [
-		// {
-		// 	"fieldname": "my_filter",
-		// 	"label": __("My Filter"),
-		// 	"fieldtype": "Data",
-		// 	"reqd": 1,
-		// },
-	],
-};
+frappe.query_reports["CRM Lead Conversion Rate"] = {};
